e991b4974ee1a8776c728e5bb4376a0ae8f729be
truesanjar <truesanjar@gmail.com>
2026-06-09 12:59:18 +0500

    commit

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig({
-  plugins: [react()],
-});
+export default {
+  build: {
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+        app: 'app/index.html'
+      }
+    }
+  }
+}
